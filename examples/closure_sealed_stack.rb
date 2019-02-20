b82d4141483fe39bfda33a74b9cecab445761b38
charkitch <charles.kitchen@gmail.com>
2019-02-20 14:48:49 -0500

    add empty? to API

--- examples/closure_sealed_stack.rb
+++ examples/closure_sealed_stack.rb
@@ -14,11 +14,16 @@ def untouchable_stack_generator
   peek = -> () do
     element = stack[-1]
     element
-  end  
+  end
+
+  empty = -> do
+    stack.empty?
+  end
   
   return { 
     add: add, 
     remove: remove, 
-    peek: peek, 
+    peek: peek,
+    empty: empty,
     }
 end  
